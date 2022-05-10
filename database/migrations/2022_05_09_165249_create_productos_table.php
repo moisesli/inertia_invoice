120a33ea41c8235b8503439f9ca9205765b1d4ff
Moises <elnaufrago2009@gmail.com>
2022-05-09 19:40:32 -0500

    editing

---
+++ database/migrations/2022_05_09_165249_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',150);
+            $table->string('codigo',20);
+            $table->decimal('precio_sin_igv',8,2);
+            $table->decimal('igv',8,2);
+            $table->decimal('precio_con_igv',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
